c66c3ce9c7a220a7e32dc183d5a75ac52f5a1b2c
SaviolaX <withcraft1717@gmail.com>
2022-08-02 18:50:43 +0300

    added new functions

--- task_about_library/film_notes/notes.py
+++ task_about_library/film_notes/notes.py
@@ -1,12 +1,5 @@
-import csv
-import os
-import pandas as pd
-
-from random import randint
-from services import create_csv_file, append_data_to_csv_file
-
-
-
+from operator import itemgetter
+from services import write_csv_file, append_data_to_csv_file, read_csv_file
 
 
 class FilmNotes():
@@ -14,37 +7,108 @@ class FilmNotes():
     def __init__(self, filename: str):
         self.filename = filename
 
-    def create(self, headers=[]):
-        """Create csv file with default headers"""
-        create_csv_file(filename=self.filename, headers=headers)
+    def create_headers(self, headers: list = []):
+        """
+            headers: list of column names for csv file
+        Create empty csv file with headers 
+        """
+        write_csv_file(filename=self.filename, headers=headers)
 
-    def add(self, name: str, note: str, rating: int):
-        """Add a new film information to current csv file"""
+    def add_row(self, name: str, note: str, rating: int):
+        """
+            name: Name of the film
+            note: Resume about the film
+            rating: Number from 1 to 5
+        
+        Add a new film information to current csv file
+        """
         if rating in range(1, 6):
             film_data = [name, note, rating]
-            append_data_to_csv_file(filename=self.filename, data=film_data)
+            append_data_to_csv_file(filename=self.filename,
+                                    film_data=film_data)
         else:
             print('Incorrect rating number, it has to be integer from 1 to 5.')
-    
-    def read(self):
-        df = pd.read_csv(f'{self.filename}.csv')
-        print(df.head())
-        # with open(f'{self.filename}.csv', 'r', encoding='utf-8') as file:
-        #     reader = csv.reader(file)
-        #     for row in reader:
-        #         print(row)
 
-    def delete(self, id:int):
-        df = pd.read_csv(f'{self.filename}.csv')
-        df.drop(df.index[id], axis=0, inplace=True)
-        print(f'Row was deleted successfully.')
+    def read_all_rows(self):
+        """
+        Read csv file and print in console
+        all rows with indexes
+        """
+        notes_lst = read_csv_file(self.filename)
+        for index, row in enumerate(notes_lst):
+            print(index, row)
+
+    def read_single_row(self, id: int):
+        """
+            id: row's index in list
+        Iterate list of rows and append the in list
+        Print in console list item by index id
+        """
+        notes_lst = read_csv_file(self.filename)
+
+        print(notes_lst[0])  # print headers
+        if id != 0:
+            print(notes_lst[id])
+        else:
+            print('[INFO] Wrong id, id starts from 1.')
+
+    def delete_row(self, id: int):
+        """
+            id: row's index in list
+        Append all rows in list,
+        Delete list item by index id,
+        Rewrite csv file without deleted item.
+        """
+        notes_lst = read_csv_file(self.filename)
+
+        if id != 0:
+            # delete item from the list
+            del notes_lst[id]
+            # recreate csv file with headers
+            write_csv_file(filename=self.filename, headers=notes_lst[0])
+            # append the rest of items from list to the recreated csv file
+            for row in notes_lst[1:]:
+                append_data_to_csv_file(filename=self.filename, film_data=row)
+        else:
+            print('[INFO] Wrong id, id starts from 1.')
+            
+    def sort_by_rate(self, asc:bool=None, desc:bool=None):
+        """
+            asc: Boolean value
+            desc: Boolean value
+            asc: => sorting list from min to max
+            desc: => sorting lost from max to min
+            default: => asc
+        """
+        # check how to sort list
+        if asc:
+            blean = False 
+        elif desc:
+            blean = True
+        else:
+            blean = False 
+        
+        # list with sorted items
+        result = []
+        notes_lst = read_csv_file(self.filename)
+        sorting_lst = notes_lst[1:]
+        sorted_lst = sorted(sorting_lst, key=itemgetter(2), reverse=blean)
+        result.append(notes_lst[0])
+        for i in sorted_lst:
+            result.append(i)
+        print(result)
+        
+
+
 
 notes = FilmNotes('My_note')
-# notes.create(['film_title', 'note', 'rating'])
-# notes.add('Калина', 'Гарний', 4)
-# notes.add('Ластівка', 'Слабкий', 2)
-# notes.add('Сонце', 'Чудовий', 5)
-# notes.add('Калина', 'Гівно', 1)
-notes.delete(id=0)
-notes.read()
-
+# notes.create_headers(['film_title', 'note', 'rating'])
+# notes.add_row('Калина', 'Гарний', 4)
+# notes.add_row('Ластівка', 'Слабкий', 2)
+# notes.add_row('Сонце', 'Чудовий', 5)
+# notes.add_row('Калина', 'Гівно', 1)
+# notes.delete_row(id=2)
+# notes.read_all_rows()
+# notes.read_single_row(id=1)
+# notes.delete_row(id=1)
+# notes.sort_by_rate()
--- task_about_library/film_notes/services.py
+++ task_about_library/film_notes/services.py
@@ -1,11 +1,22 @@
 import csv
 
-def create_csv_file(filename, headers):
+
+def write_csv_file(filename: str, headers:list) -> object:
     with open(f'{filename}.csv', 'w', encoding='utf-8', newline='') as file:
-        wrirer = csv.writer(file)
-        wrirer.writerow(headers)
-        
-def append_data_to_csv_file(filename:str, data:list):
+        writer = csv.writer(file)
+        writer.writerow(headers)
+
+
+def append_data_to_csv_file(filename: str, film_data:list) -> object:
     with open(f'{filename}.csv', 'a', encoding='utf-8', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(data)
+        writer = csv.writer(file)
+        writer.writerow(film_data)
+
+
+def read_csv_file(filename: str) -> list:
+    with open(f'{filename}.csv', 'r', encoding='utf-8', newline='') as file:
+        reader = csv.reader(file)
+        lst = []
+        for row in reader:
+            lst.append(row)
+    return lst
